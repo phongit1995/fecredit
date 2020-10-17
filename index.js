0dc8838281de8261e89d1c7dea0befcbcf776262
phongit <phongit1995@gmail.com>
2020-10-17 11:46:52 +0700

    add index

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ let feCreditRoute = require("./router/fecredit");
 app.use(cros());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.get("/",function(req,res){
+    res.send("Hello");
+})
 app.use("/fecredit",feCreditRoute);
 app.listen(process.env.PORT||3000,function(){
     console.log("App running On Port : " + process.env.PORT||3000)
