b685bb13b8dbd2813d72b2c14ad81ef7d7786324
phong  nguyen <phongit1995@gmail.com>
2020-10-23 17:39:43 +0700

    add comment

--- router/cricModel.js
+++ router/cricModel.js
@@ -25,12 +25,14 @@ const loginToWeb = async ()=>{
     try {
         let data  = await request(options);
     } catch (error) {
-        console.log(error.response.headers['set-cookie']);
         result =error.response.headers['set-cookie'].join(";");
         let cookies =parseCookie(result);
         let newCookie = cookie.parse(cookies);
         newCookie = cookieArrayPase(newCookie);
-        return newCookie;
+        return {
+            newCookie,
+            cookies
+        };
     }
 }
 const getDataSearch =async (cookie,cmnd)=>{
@@ -43,68 +45,74 @@ const getDataSearch =async (cookie,cmnd)=>{
     ]});
     const page = await browser.newPage();
     await page.setViewport({ width: 2000, height: 2000});
-    await page.setCookie(...cookie);
+    await page.setCookie(...cookie.newCookie);
     await page.goto("https://cic.org.vn/webcenter/portal/CMSPortal/page1422/page1996",
     {
             waitUntil: 'networkidle2',
     });
-    await page.evaluate((cmnd) => {
-        document.getElementById("T:oc_5065589220region1:soc10::content").selectedIndex = 0;
-        document.getElementById("T:oc_5065589220region1:strLoaiKH::content").selectedIndex = 1;
-        document.getElementById("T:oc_5065589220region1:txtsocmt::content").value=cmnd ;
-    },cmnd);
-    let element = await page.$('[id="T:oc_5065589220region1:btntimkiemkh"]');
-    await Promise.all([
-        element.click(),
-        waitForNetworkIdle(page, 100, 0) // equivalent to 'networkidle0'
-    ]);
-        let data = await page.evaluate(() => document.querySelector('*').outerHTML);
-    let $ = cheerio.load(data);
-    let cic = $(".x10q>tbody>tr:nth-child(1)>td:nth-child(2)").text();
-    if(!cic){
-        await browser.close();
-        throw "NOT_FOUND_CIC";
-    }
-    console.log("cic la: " + cic);
-    await page.goto("https://cic.org.vn/webcenter/portal/CMSPortal?_afrLoop=503527279814704#%2Foracle%2Fwebcenter%2Fpage%2FscopedMD%2Fs774abad3_0d36_4122_a489_ccf21aaa3a66%2FPage225.jspx%40%3F_adf.ctrl-state%3Drgq4sieej_4",
-    {
-        waitUntil: 'networkidle2'
+    let action = await page.evaluate(()=>{
+        return document.getElementById("f1").action;
     })
-    const InputCic=await page.$('[id="T:oc_6025695556region1:txtmacic::content"]');
-    await InputCic.focus();
-    await page.keyboard.type(cic);
-    await Promise.all([
-        page.focus('[id="T:oc_6025695556region1:txtdkkd::content"]'),
-        waitForNetworkIdle(page, 100, 0) // equivalent to 'networkidle0'
-    ]);
-    let buttonSearch = await page.$('[id="T:oc_6025695556region1:btnhotrotimkiem"]');
-    await Promise.all([
-        buttonSearch.click(),
-        waitForNetworkIdle(page, 300, 0) // equivalent to 'networkidle0'
-    ]);
-    let buttonYes = await page.$('[id="T:oc_6025695556region1:tnYes"]');
-    await Promise.all([
-        buttonYes.click()
-    ]);
-    let result = await page.evaluate(()=>{
-        let cic = document.getElementById("T:oc_6025695556region1:txtmacic::content").value ;
-        let dkkd = document.getElementById("T:oc_6025695556region1:txtdkkd::content").value ;
-        let cmnd = document.getElementById("T:oc_6025695556region1:txtcmnd::content").value ;
-        let ten = document.getElementById("T:oc_6025695556region1:txttenkh::content").value ;
-        let diachi = document.getElementById("T:oc_6025695556region1:txtdiachi::content").value ;
-        let dienthoai = document.getElementById("T:oc_6025695556region1:txtdienthoai::content").value ;
-        let masothue = document.getElementById("T:oc_6025695556region1:txtmsthue::content").value ;
-        let tgd = document.getElementById("T:oc_6025695556region1:txttgd::content").value ;
-        return {
-            cic,dkkd,cmnd,ten,diachi,dienthoai,masothue,tgd
-        }
-    })
-    let dataHtml = await page.evaluate(() => document.querySelector('*').outerHTML);
-    $= cheerio.load(dataHtml);
-    let thongbao = $("tbody > tr:nth-child(4) > td.x51 > span").text();
-    await page.close();
-    return result ;
-    await browser.close();
+    const cookies = await page.cookies();
+    console.log(cookies);
+    console.log(action);
+    // await page.evaluate((cmnd) => {
+    //     document.getElementById("T:oc_5065589220region1:soc10::content").selectedIndex = 0;
+    //     document.getElementById("T:oc_5065589220region1:strLoaiKH::content").selectedIndex = 1;
+    //     document.getElementById("T:oc_5065589220region1:txtsocmt::content").value=cmnd ;
+    // },cmnd);
+    // let element = await page.$('[id="T:oc_5065589220region1:btntimkiemkh"]');
+    // await Promise.all([
+    //     element.click(),
+    //     waitForNetworkIdle(page, 100, 0) // equivalent to 'networkidle0'
+    // ]);
+    //     let data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    // let $ = cheerio.load(data);
+    // let cic = $(".x10q>tbody>tr:nth-child(1)>td:nth-child(2)").text();
+    // if(!cic){
+    //     await browser.close();
+    //     throw "NOT_FOUND_CIC";
+    // }
+    // console.log("cic la: " + cic);
+    // await page.goto("https://cic.org.vn/webcenter/portal/CMSPortal?_afrLoop=503527279814704#%2Foracle%2Fwebcenter%2Fpage%2FscopedMD%2Fs774abad3_0d36_4122_a489_ccf21aaa3a66%2FPage225.jspx%40%3F_adf.ctrl-state%3Drgq4sieej_4",
+    // {
+    //     waitUntil: 'networkidle2'
+    // })
+    // const InputCic=await page.$('[id="T:oc_6025695556region1:txtmacic::content"]');
+    // await InputCic.focus();
+    // await page.keyboard.type(cic);
+    // await Promise.all([
+    //     page.focus('[id="T:oc_6025695556region1:txtdkkd::content"]'),
+    //     waitForNetworkIdle(page, 100, 0) // equivalent to 'networkidle0'
+    // ]);
+    // let buttonSearch = await page.$('[id="T:oc_6025695556region1:btnhotrotimkiem"]');
+    // await Promise.all([
+    //     buttonSearch.click(),
+    //     waitForNetworkIdle(page, 300, 0) // equivalent to 'networkidle0'
+    // ]);
+    // let buttonYes = await page.$('[id="T:oc_6025695556region1:tnYes"]');
+    // await Promise.all([
+    //     buttonYes.click()
+    // ]);
+    // let result = await page.evaluate(()=>{
+    //     let cic = document.getElementById("T:oc_6025695556region1:txtmacic::content").value ;
+    //     let dkkd = document.getElementById("T:oc_6025695556region1:txtdkkd::content").value ;
+    //     let cmnd = document.getElementById("T:oc_6025695556region1:txtcmnd::content").value ;
+    //     let ten = document.getElementById("T:oc_6025695556region1:txttenkh::content").value ;
+    //     let diachi = document.getElementById("T:oc_6025695556region1:txtdiachi::content").value ;
+    //     let dienthoai = document.getElementById("T:oc_6025695556region1:txtdienthoai::content").value ;
+    //     let masothue = document.getElementById("T:oc_6025695556region1:txtmsthue::content").value ;
+    //     let tgd = document.getElementById("T:oc_6025695556region1:txttgd::content").value ;
+    //     return {
+    //         cic,dkkd,cmnd,ten,diachi,dienthoai,masothue,tgd
+    //     }
+    // })
+    // let dataHtml = await page.evaluate(() => document.querySelector('*').outerHTML);
+    // $= cheerio.load(dataHtml);
+    // let thongbao = $("tbody > tr:nth-child(4) > td.x51 > span").text();
+    // await page.close();
+    // return result ;
+     await browser.close();
     
 }
 module.exports = {
